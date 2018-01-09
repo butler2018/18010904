3bb4dc381926c0bba843df03e0ef8d51f88d1ba0
butle <butler2018@hotmail.com>
2018-01-09 22:30:57 +0800

    ver2

--- app/src/main/java/com/example/student/a18010904/MainActivity.java
+++ app/src/main/java/com/example/student/a18010904/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.student.a18010904;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.lang.annotation.Target;
 
 public class MainActivity extends AppCompatActivity {
    String idLove = "LOVE";
@@ -15,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= 26) {
+            regChannel();
+        }
+
+    }
+    @TargetApi(Build.VERSION_CODES.O)
+    public void regChannel() {
+
         channelLove = new NotificationChannel(
                 idLove,
                 "Channel Love",
@@ -23,17 +39,36 @@ public class MainActivity extends AppCompatActivity {
         channelLove.enableLights(true);
         channelLove.enableVibration(true);
 
-        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         nm.createNotificationChannel(channelLove);
-
     }
+    @TargetApi(Build.VERSION_CODES.O)
     public void click1(View v)
     {
-        Notification.Builder builder = new Notification.Builder(MainActivity.this,
-        idLove);
+        Notification.Builder builder;
+        if(Build.VERSION.SDK_INT >= 26)
+        {
+            builder  = new Notification.Builder(MainActivity.this,idLove);
+        }
+        else
+        {
+            builder  = new Notification.Builder(MainActivity.this);
+        }
+
+        Intent it = new Intent(MainActivity.this, infoActivity.class);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP |
+        Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this,123
+        ,it,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
         builder.setContentTitle("測試");
         builder.setContentText("這是內容");
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+       // builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pi);
+
+
         Notification notify = builder.build();
         nm.notify(1,notify);
 
